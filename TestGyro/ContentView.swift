5631d01cb46a66b206390a5fe3bb6fe6b29c05af
Azuar <Lazuardhiimaniahfar@gmail.com>
2024-04-28 22:38:48 +0700

    Initial Commit

---
+++ TestGyro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TestGyro
+//
+//  Created by Lazuardhi Imani Ahfar on 28/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TestGyro/TestGyroApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGyroApp.swift
+//  TestGyro
+//
+//  Created by Lazuardhi Imani Ahfar on 28/04/24.
+//
+
+import SwiftUI
+
+@main
+struct TestGyroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
