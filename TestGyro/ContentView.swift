a519a41eb8c6e7831f7a6a19b1f6e53d79d8b6da
Azuar <Lazuardhiimaniahfar@gmail.com>
2024-04-29 14:15:11 +0700

    test1

--- TestGyro/ContentView.swift
+++ TestGyro/ContentView.swift
@@ -5,19 +5,33 @@
 //  Created by Lazuardhi Imani Ahfar on 28/04/24.
 //
 
+import CoreMotion
 import SwiftUI
 
+
+
 struct ContentView: View {
+    
+    let motionManager = CMMotionManager()
+    var gyroData: CMRotationRate = CMRotationRate(x: 0, y: 0, z: 0)
+    
+    
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            Text("Rotation Rate:")
+            Text("X: \(gyroData.x)")
+            Text("Y: \(gyroData.y)")
+            Text("Z: \(gyroData.z)")
+            
         }
         .padding()
     }
 }
+
 
 #Preview {
     ContentView()
---
+++ TestGyro/GyroView.swift
@@ -0,0 +1,12 @@
+//
+//  GyroView.swift
+//  TestGyro
+//
+//  Created by Lazuardhi Imani Ahfar on 29/04/24.
+//
+
+//import CoreMotion
+//import SwiftUI
+//
+//
+//func gyro
